e9d1384130c05e680595f0ced3e5a5cdc8350276
Caden <caden7700@gmail.com>
2020-08-30 23:47:50 -0400

    Initial commit

---
+++ lib/elixir_neural_net.ex
@@ -0,0 +1,17 @@
+defmodule ElixirNeuralNet do
+    def main do
+        inputs = [1,2,3]
+        weights = [4,5,6]
+        bias = 3
+        propagate_sum(inputs, weights, bias)
+    end
+
+    # Inputs * Weights + bias
+    def propagate_sum(inputs, weights, bias) do
+        Enum.zip(inputs, weights)
+        |> Enum.map(fn {x,y} -> x * y end)
+        |> Enum.sum()
+        # Cannot pipe into addition without Kernel namespace
+        |> Kernel.+(bias)
+    end 
+end
---
+++ test/elixir_neural_net_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirNeuralNetTest do
+  use ExUnit.Case
+  doctest ElixirNeuralNet
+
+  test "greets the world" do
+    assert ElixirNeuralNet.hello() == :world
+  end
+end
